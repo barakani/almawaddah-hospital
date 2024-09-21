3db0e17876205d050ccdf117cc7a137823cc037e
Dawood Albarakani <125603585+barakani@users.noreply.github.com>
2024-09-21 21:24:24 +0300

    delete

--- _next/static/static/WE7ckMd4iIfZFg3Aj6CJq/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fdepartments","\u002F[locale]\u002Fdepartments\u002F[slug]","\u002F[locale]\u002Fequipments","\u002F[locale]\u002Fopd","\u002F[locale]\u002Fopd\u002F[slug]","\u002F[locale]\u002Fpartners"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/static/chunks/app/page-4dcc302f5aff470d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,185],{5111:function(){}},function(n){n.O(0,[139,511,744],function(){return n(n.s=5111)}),_N_E=n.O()}]);
